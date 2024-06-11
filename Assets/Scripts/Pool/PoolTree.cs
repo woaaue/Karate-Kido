d4106188abbd79447078cf597c7fb112807fff2a
woaaue <safat200@mail.ru>
2024-06-11 08:40:16 +0300

    edit duplicate fields

--- Assets/Scripts/Pool/PoolTree.cs
+++ Assets/Scripts/Pool/PoolTree.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 
 public sealed class PoolTree : PoolMono<Tree>
 {
-    [SerializeField] private Tree _prefab;
+    [SerializeField] private Tree _treePrefab;
     [SerializeField] private int _count;
     [SerializeField] private Transform _parent;
 
-    private void Start() => Initialize(_prefab, _parent, _count);
+    private void Start() => Initialize(_treePrefab, _parent, _count);
 
     public List<Tree> GetPool()
     {
