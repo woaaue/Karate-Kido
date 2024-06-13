79285da8677e53b3a78e87c3157b2ad43ae83e94
woaaue <safat200@mail.ru>
2024-06-13 12:45:34 +0300

    edit popup

--- Assets/Scripts/Popups/EndGamePopup.cs
+++ Assets/Scripts/Popups/EndGamePopup.cs
@@ -1,17 +1,33 @@
 using TMPro;
+using Zenject;
 using UnityEngine;
 
 public sealed class EndGamePopup : Popup<EndGamePopupSettings>
 {
     [SerializeField] private TextMeshProUGUI _textInfo;
+    [SerializeField] private TextMeshProUGUI _bestResult;
+
+    private ScoreService _scoreService;
+
+    [Inject]
+    public void Construct(ScoreService scoreService)
+    {
+        _scoreService = scoreService;
+        GetResult();
+    }
 
     public override void Setup(EndGamePopupSettings settings)
     {
-        _textInfo.text = settings.Action;
+        _textInfo.text = settings.InfoText;
+    }
+
+    private void GetResult()
+    {
+        _bestResult.text = _scoreService.GetData().BestScore.ToString();
     }
 }
 
 public sealed class EndGamePopupSettings : PopupBaseSettings
 {
-    public string Action;
+    public string InfoText;
 }
