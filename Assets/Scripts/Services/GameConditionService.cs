314f2edd254d4ee0e1d3725cd23ce972dace7b9f
woaaue <safat200@mail.ru>
2024-06-14 15:49:25 +0300

    create game condition service

---
+++ Assets/Scripts/Services/GameConditionService.cs
@@ -0,0 +1,32 @@
+using Zenject;
+using UnityEngine;
+using JetBrains.Annotations;
+
+public sealed class GameConditionService : MonoBehaviour
+{
+    private TreeService _treeService;
+    private ScoreService _scoreService;
+    private TimerService _timerService;
+
+    [Inject]
+    public void Construct(ScoreService scoreService, TimerService timerService, TreeService treeService)
+    {
+        _treeService = treeService;
+        _scoreService = scoreService;
+        _timerService = timerService;
+    }
+
+    [UsedImplicitly]
+    public void RestartGame()
+    {
+        _treeService.ScipTree();
+        _scoreService.ResetScore();
+        _timerService.StartTimer();
+    }
+
+    [UsedImplicitly]
+    public void StartGame()
+    {
+        _timerService.StartTimer();
+    }
+}
