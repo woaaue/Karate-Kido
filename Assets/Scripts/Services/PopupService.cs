9e7c3dd1b2d947afa5f3194928ac9bfeb7328ec1
woaaue <safat200@mail.ru>
2024-06-14 15:49:44 +0300

    minor add popup system

---
+++ Assets/Scripts/Services/PopupService.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public sealed class PopupService : MonoBehaviour
+{
+    [SerializeField] private GameService _gameService;
+    [SerializeField] private TimerService _timerService;
+    [SerializeField] private PopupController _popupController;
+
+    private void Start()
+    {
+        ShowStartGamePopup();
+
+        _timerService.OnUpTimed += ShowEndGamePopup;
+        _gameService.OnGameEnded += ShowEndGamePopup;
+    }
+
+    private void ShowStartGamePopup()
+    {
+        _popupController.ShowPopup(new StartGamePopupSettings());
+    }
+
+    private void ShowEndGamePopup(string message)
+    {
+        _timerService.StopTimer();
+
+        _popupController.ShowPopup(new EndGamePopupSettings
+        {
+            InfoText = message,
+        });
+    }
+
+}
