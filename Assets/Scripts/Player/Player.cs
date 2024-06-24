1e3b3d41af6142f0a7a82d15c09fc44d26f7e9eb
woaaue <safat200@mail.ru>
2024-06-25 00:35:36 +0300

    edit player system

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,4 +1,5 @@
 using System;
+using Zenject;
 using UnityEngine;
 using Scripts.Control;
 
@@ -9,37 +10,34 @@ public sealed class Player : MonoBehaviour
 
     public event Action<Vector2> OnPlayerMoved;
 
-    private GameService _gameService;
+    [Inject]private GameService _gameService;
 
     private void Start()
     {
         _clickHandler.OnDirectionChanged += ChangePosition;
+        _gameService.OnPlayerHited += Hit;
         //_gameService.OnPlayerDied +=
-        //_gameService.OnPlayerHit +=
     }
 
     private void OnDestroy()
     {
         _clickHandler.OnDirectionChanged -= ChangePosition;
+        _gameService.OnPlayerHited -= Hit;
         //_gameService.OnPlayerDied -=
-        //_gameService.OnPlayerHit -=
     }
 
-    public void Construct(GameService gameService)
-    {
-        _gameService = gameService;
-    }
+    private void Hit() => _animator.PlayAttack();
 
     private void ChangePosition(float xPosition)
     {
         if (xPosition < 0)
         {
-            transform.position = new Vector3(-1.3f, 0);
+            transform.position = new Vector3(-1.3f, transform.position.y);
             transform.rotation = new Quaternion(0, -180, 0, 0);
         }
         else
         {
-            transform.position = new Vector3(1.3f, 0);
+            transform.position = new Vector3(1.3f, transform.position.y);
             transform.rotation = Quaternion.identity;
         }
 
--- Assets/Scripts/Player/PlayerAnimator.cs
+++ Assets/Scripts/Player/PlayerAnimator.cs
@@ -1,6 +1,12 @@
 using UnityEngine;
+using JetBrains.Annotations;
 
 public sealed class PlayerAnimator : MonoBehaviour
 {
-    
+    [SerializeField] private Animator _animator;
+
+    [UsedImplicitly]
+    public void PlayIdle() => _animator.Play("Idle");
+
+    public void PlayAttack() => _animator.Play("PlayerAttack");
 }
