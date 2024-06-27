cbadd6a78e54b18866e6363a38452099de528eca
woaaue <safat200@mail.ru>
2024-06-28 02:07:14 +0300

    saves selected language

--- Assets/Scripts/Localization/SwitcherLanguage.cs
+++ Assets/Scripts/Localization/SwitcherLanguage.cs
@@ -1,13 +1,20 @@
+using System;
 using UnityEngine;
 using JetBrains.Annotations;
 using UnityEngine.Localization.Settings;
 using UnityEngine.ResourceManagement.AsyncOperations;
 
+[Serializable]
+public sealed class Language
+{
+    public int LanguageIndex;
+}
+
 public sealed class SwitcherLanguage : MonoBehaviour
 {
     private int _currentLocalIndex = 0;
 
-    private void Start() => _currentLocalIndex = GetCurrentIndexLocale();
+    private void Start() => LoadSelectLanguage();
 
     [UsedImplicitly]
     public void SwitchLocale() => InitializeLocalization();
@@ -34,7 +41,7 @@ public sealed class SwitcherLanguage : MonoBehaviour
         _currentLocalIndex = (_currentLocalIndex + 1) % locales.Count;
         LocalizationSettings.SelectedLocale = locales[_currentLocalIndex];
 
-        Debug.Log($"Language switched to: {locales[_currentLocalIndex].Identifier.Code}");
+        SaveSelectLanguage();
     }
 
     private int GetCurrentIndexLocale()
@@ -44,4 +51,27 @@ public sealed class SwitcherLanguage : MonoBehaviour
 
         return locales.Locales.IndexOf(currentLocale);
     }
+
+    private Language GetLanguage()
+    {
+        var currentLanguage = new Language
+        {
+            LanguageIndex = GetCurrentIndexLocale(),
+        };
+
+        return currentLanguage;
+    }
+
+    private void SetLanguage(Language language)
+    {
+        if (language != null)
+            _currentLocalIndex = language.LanguageIndex;
+        else
+            _currentLocalIndex = GetCurrentIndexLocale();
+
+        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[_currentLocalIndex];
+    }
+
+    private void SaveSelectLanguage() => Storage.Save(GetLanguage(), "currentLanguage.json");
+    private void LoadSelectLanguage() => SetLanguage(Storage.Load<Language>("currentLanguage.json"));
 }
